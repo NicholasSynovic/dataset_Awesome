8a7831cfa124b188897faa3f9f14d63f06bee718
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-20 16:22:35 -0500

    Create list database schema

--- src/db.py
+++ src/db.py
@@ -1,1 +1,26 @@
-from sqlalchemy import MetaData, Table
+from sqlalchemy import (
+    Column,
+    Engine,
+    Integer,
+    MetaData,
+    PrimaryKeyConstraint,
+    String,
+    Table,
+)
+
+
+class DB:
+    def __init__(self, dbConn: Engine) -> None:
+        self.dbConn: Engine = dbConn
+
+        metadata: MetaData = MetaData()
+
+        listTableSchema: Table = Table(
+            "lists",
+            metadata,
+            Column("id", Integer),
+            Column("author", String),
+            Column("name", String),
+            Column("url", String),
+            PrimaryKeyConstraint("id"),
+        )
